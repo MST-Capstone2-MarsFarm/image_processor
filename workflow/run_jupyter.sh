de171bc38142accac2ab412f77606d406db62129
ajc3xc <ajc3xc@umsystem.edu>
2024-03-11 21:24:42 -0500

    figured out how to encapsulate running jupyter lab

--- workflow/run_jupyter.sh
+++ workflow/run_jupyter.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# ACTIVATE ANACONDA
-eval "$(conda shell.bash hook)"
-conda activate plantenv_pcv4_jupyter
+#make sure you are in the same directory as this file when running it
+run_jupyter()
+{
+    # ACTIVATE ANACONDA
+    eval "$(conda shell.bash hook)"
+    conda activate plantenv_pcv4_jupyter
 
-srun --time=1-00:00:00 -p general --job-name=jupyterlab jupyter lab --no-browser --port=8888 > /dev/null 2>&1 &
-#get node name that you most recently submitted
-compute_node_name="$(squeue --noheader -u $(whoami) --format=%N | tail -n 1)"
-sleep 5
-#this should work if you don't submit any other sbatch jobs simultaneously
-echo "use this node name for the ssh command to run jupyter on a browser"
-echo $compute_node_name
-echo "$(squeue --noheader -u $(whoami) --format=%N | tail -n 1)"
-echo 
-wait
+    echo $(hostname)
+    jupyter lab --no-browser --port=8888 > /dev/null 2>&1
+}
+
+export -f run_jupyter
+
+#encapsulate the rest of the code within the bash function
+srun --time=1-00:00:00 -p general --job-name=jupyterlab bash -c 'run_jupyter'
